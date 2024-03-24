e54183a486b882f4c1362680bac3dfb72ad00c88
Thibaud Balem <44678337+tbalem@users.noreply.github.com>
2024-03-24 14:58:35 +0100

    refactor: user input handling and its error handling

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,33 @@ use std::io::{self, Write};
 
 /// Represents the possible errors that can occur during user input.
 enum UserInputError {
-    IoError(io::Error), // Error while performing I/O operations.
-    UnexpectedLength,   // The input does not correspond to the defined length.
-    NotAlphabetic,      // The input contains non-alphabetic characters.
+    UnexpectedLength, // The input does not correspond to the defined length.
+    NotAlphabetic,    // The input contains non-alphabetic characters.
 }
 
-/// Reads a line of user input from the standard input and performs validation.
-/// Returns the trimmed and uppercased input if it is valid, or an error otherwise.
-fn get_user_input(expected_length: usize) -> Result<String, UserInputError> {
-    let mut input = String::new();
-    println!("Please input a new guess:");
-    io::stdout().flush().map_err(UserInputError::IoError)?;
+/// Formats and checks the input string for validity.
+/// Returns the formatted string if it is valid, or an error otherwise.
+fn format_and_check_input_string(
+    input_str: String,
+    expected_length: usize,
+) -> Result<String, UserInputError> {
+    let trimmed_input_str = input_str.trim();
 
-    io::stdin()
-        .read_line(&mut input)
-        .map_err(UserInputError::IoError)?; // Error while reading user input.
-
-    let trimmed_uppercased_input = input.trim();
-
-    if trimmed_uppercased_input.chars().count() != expected_length {
+    if trimmed_input_str.chars().count() != expected_length {
         Err(UserInputError::UnexpectedLength) // Your guess must be exactly 5 characters.
-    } else if !trimmed_uppercased_input
-        .chars()
-        .all(|c| c.is_ascii_alphabetic())
-    {
+    } else if !trimmed_input_str.chars().all(|c| c.is_ascii_alphabetic()) {
         Err(UserInputError::NotAlphabetic) // Some of your input is not an alphabetic character.
     } else {
-        Ok(trimmed_uppercased_input.to_ascii_uppercase())
+        Ok(trimmed_input_str.to_ascii_uppercase())
     }
+}
+
+/// Reads user input from stdin.
+fn get_user_input_stdin() -> Result<String, io::Error> {
+    let mut input = String::new();
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+    Ok(input)
 }
 
 /// Checks for perfect characters in the guess word and updates the character counts.
@@ -95,32 +94,34 @@ fn game_loop(guess_word: &String) {
 
     let mut edited_char_counts = char_counts.clone();
 
-    match get_user_input(guess_word.len()) {
-        Ok(trimmed_uppercased_input) => {
-            let results_with_perfect_characters = check_perfect_characters(
-                &guess_word,
-                &trimmed_uppercased_input,
-                &mut edited_char_counts,
-            );
+    println!("Please input a new guess:");
+    match get_user_input_stdin() {
+        Ok(user_input_str) => match format_and_check_input_string(user_input_str, guess_word.len())
+        {
+            Ok(trimmed_uppercased_input) => {
+                let results_with_perfect_characters = check_perfect_characters(
+                    &guess_word,
+                    &trimmed_uppercased_input,
+                    &mut edited_char_counts,
+                );
 
-            let results = check_misplaced_characters(
-                &guess_word,
-                &trimmed_uppercased_input,
-                edited_char_counts,
-                results_with_perfect_characters,
-            );
+                let results = check_misplaced_characters(
+                    &guess_word,
+                    &trimmed_uppercased_input,
+                    edited_char_counts,
+                    results_with_perfect_characters,
+                );
 
-            println!("{}", results.into_iter().collect::<String>());
-        }
-        Err(UserInputError::IoError(err)) => {
-            eprintln!("Error while reading input: {}", err);
-        }
-        Err(UserInputError::UnexpectedLength) => {
-            println!("Your guess must be exactly 5 characters.");
-        }
-        Err(UserInputError::NotAlphabetic) => {
-            println!("Some of your input is not an alphabetic character.");
-        }
+                println!("{}", results.into_iter().collect::<String>());
+            }
+            Err(UserInputError::UnexpectedLength) => {
+                println!("Your guess must be exactly 5 characters.");
+            }
+            Err(UserInputError::NotAlphabetic) => {
+                println!("Some of your input is not an alphabetic character.");
+            }
+        },
+        Err(io_error) => eprintln!("Error while reading user input: {}", io_error),
     }
 }
 
