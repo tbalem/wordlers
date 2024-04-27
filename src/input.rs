478032e3e89cff0b0ca7d39c49195f69ed7b2d03
Thibaud Balem <44678337+tbalem@users.noreply.github.com>
2024-04-27 20:09:04 +0200

    docs: add error info and example for get_user_input

--- src/input.rs
+++ src/input.rs
@@ -3,6 +3,14 @@ use std::io;
 pub mod format_and_check;
 
 /// Reads user input from stdin.
+/// # Errors
+/// Returns an error if the input cannot be read
+/// # Examples
+/// ```
+/// use wordlers::input::get_user_input_stdin;
+///
+/// let user_input = get_user_input_stdin().unwrap();
+/// ```
 pub fn get_user_input_stdin() -> Result<String, io::Error> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
