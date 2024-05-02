8ed1260f66060bf5741ef2c7aa9c3d13daa27eb3
Thibaud Balem <44678337+tbalem@users.noreply.github.com>
2024-05-02 11:08:29 +0200

    feat: add colored and display character with its result as a color instead of X/O

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use core::fmt;
 use std::error::Error;
 use wordlers::game_logic::{analyze_guess, CharacterState};
@@ -8,7 +9,8 @@ const MAX_IOERROR_TRIES: usize = 5;
 /// Represents the possible errors that can occur during a guess iteration.
 #[derive(Debug)]
 pub enum GuessIterationError {
-    TooManyIOErrorIteration, // There have been
+    // There have been too many IOError (defined by MAX_IOERROR_TRIES)
+    TooManyIOErrorIteration,
 }
 
 impl Error for GuessIterationError {}
@@ -23,6 +25,22 @@ impl fmt::Display for GuessIterationError {
     }
 }
 
+#[derive(Clone)]
+struct DisplayedCharacterState {
+    character: String,
+    character_state: CharacterState,
+}
+
+impl fmt::Display for DisplayedCharacterState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.character_state {
+            CharacterState::Undefined => write!(f, "{}", self.character),
+            CharacterState::Misplaced => write!(f, "{}", self.character.yellow()),
+            CharacterState::Good => write!(f, "{}", self.character.green()),
+        }
+    }
+}
+
 /// Gives at most `n_tries` to the user to guess the `guess_word`.
 /// Also prints previous attempts' result.
 ///
@@ -30,8 +48,16 @@ impl fmt::Display for GuessIterationError {
 /// True if the user guessed the word, false otherwise.
 #[must_use]
 pub fn game_iteration(guess_word: &str, n_tries: usize) -> bool {
-    let mut guess_tries: Vec<Vec<CharacterState>> = (0..n_tries)
-        .map(|_| vec![CharacterState::Undefined; guess_word.len()])
+    let mut guess_tries: Vec<Vec<DisplayedCharacterState>> = (0..n_tries)
+        .map(|_| {
+            vec![
+                DisplayedCharacterState {
+                    character: String::from("-"),
+                    character_state: CharacterState::Undefined
+                };
+                guess_word.len()
+            ]
+        })
         .collect();
     for i in 0..n_tries {
         match guess_iteration(guess_word) {
@@ -39,19 +65,15 @@ pub fn game_iteration(guess_word: &str, n_tries: usize) -> bool {
                 guess_tries[i] = result;
                 println!("Current tries:");
                 for guess_try in &guess_tries {
-                    println!(
-                        "{}",
-                        guess_try
-                            .iter()
-                            .map(|character_state| match character_state {
-                                CharacterState::Good => 'X',
-                                CharacterState::Misplaced => 'O',
-                                CharacterState::Undefined => '-',
-                            })
-                            .collect::<String>()
-                    );
+                    for displayed_guess_character in guess_try {
+                        print!("{displayed_guess_character}");
+                    }
+                    println!();
                 }
-                if guess_tries[i].iter().all(|c| *c == CharacterState::Good) {
+                if guess_tries[i]
+                    .iter()
+                    .all(|c| c.character_state == CharacterState::Good)
+                {
                     return true;
                 }
             }
@@ -71,14 +93,21 @@ pub fn game_iteration(guess_word: &str, n_tries: usize) -> bool {
 /// # Errors
 /// Throw `GuessIterationError::TooManyIOErrorIteration` if there where more then `MAX_IOERROR_TRIES` tries
 /// with error while reading user's input.
-pub fn guess_iteration(guess_word: &str) -> Result<Vec<CharacterState>, Box<dyn Error>> {
+fn guess_iteration(guess_word: &str) -> Result<Vec<DisplayedCharacterState>, Box<dyn Error>> {
     let mut nb_incorrect_tries = 0;
     while nb_incorrect_tries < MAX_IOERROR_TRIES {
         println!("Please input a new guess:");
         match get_user_input_stdin() {
             Ok(user_input_str) => match input_string(&user_input_str, guess_word.len()) {
                 Ok(trimmed_uppercased_input) => {
-                    return Ok(analyze_guess(guess_word, &trimmed_uppercased_input));
+                    return Ok(analyze_guess(guess_word, &trimmed_uppercased_input)
+                        .into_iter()
+                        .zip(trimmed_uppercased_input.chars())
+                        .map(|(character_state, character)| DisplayedCharacterState {
+                            character: String::from(character),
+                            character_state: character_state.clone(),
+                        })
+                        .collect());
                 }
                 Err(err) => eprintln!("{err}"),
             },
